c3a72a7134203c9966d8fdcd1de649686b353faa
Egor Lavysh <egorlavys95@gmail.com>
2026-02-04 16:11:41 +0300

    fix ad v2

--- src/utils/advertisment.py
+++ src/utils/advertisment.py
@@ -2,74 +2,74 @@ POP_UPS = {
     1: {"text": """Хочешь купить игровую валюту без переплат?
 На GG.Store цены ниже среднего, а каждая сделка защищена.
 Пополнение Steam — всего 9% комиссии 🔥
-Проверяй: https://gg.store
+Проверяй: https://gg.markets/s-TeamSeek
 Вступай: https://t.me/ggstore_hub
 """,
 "pic": "AgACAgIAAxkBAAJMumlOdF4Ebno8yQdPbyevxvFoIP0EAAJCDmsbd-d4Smn6oB0aYRiRAQADAgADeQADNgQ",
-"link": "https://gg.store"},
+"link": "https://gg.markets/s-TeamSeek"},
 
     2: {"text": """Не хочешь тратить недели на фарм?
 Закажи прокачку аккаунта на GG.Store — быстро, безопасно и с поддержкой 24/7 ⚔
-Выбирай услугу: https://gg.store
+Выбирай услугу: https://gg.markets/h-TeamSeek
 Канал: https://t.me/ggstore_hub""",
 "pic": "AgACAgIAAxkBAAJMvGlOdG5nELAzvZgw2I5y_THZBtr2AAJDDmsbd-d4Sopd_QhQpiffAQADAgADeQADNgQ",
-"link": "https://gg.store"},
+"link": "https://gg.markets/h-TeamSeek"},
 
     3: {"text": """Хочешь фармить меньше, а играть больше?
 Покупай валюту и услуги прокачки на GG.Store — проверенные продавцы и быстрые сделки ⚡
-Забрать выгоду: https://gg.store
+Забрать выгоду: https://gg.markets/h-TeamSeek
 Присоеденяйся: https://t.me/ggstore_hub""",
 "pic": "AgACAgIAAxkBAAJMvmlOdJmv3aZtynXVPngOIjkc0pWtAAJKDmsbd-d4Sipxg6Pys407AQADAgADeQADNgQ",
-"link": "https://gg.store"},
+"link": "https://gg.markets/h-TeamSeek"},
 
     4: {"text": """Нужно быстро пополнить Steam?
 Заливай Стим через GG.Store — у нас фиксированные 9%, быстрые операции и безопасные платежи 🎮
-Пополнить: https://gg.store
+Пополнить: https://gg.markets/s-TeamSeek
 Канал: https://t.me/ggstore_hub
 """,
 "pic": "AgACAgIAAxkBAAJMxGlOdLUrHE2KT9OWZA2u2TCrzaCJAAJMDmsbd-d4SsWfPP7euaLqAQADAgADeQADNgQ",
-"link": "https://gg.store"},
+"link": "https://gg.markets/s-TeamSeek"},
 
     5: {"text": """🎁 У нас идёт розыгрыш!
 Среди призов — Porsche Panamera и новый игровой ПК.
-Узнай, как участвовать 👉 https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot
+Узнай, как участвовать 👉 https://gg.markets/g-TeamSeek
 Наш канал: https://t.me/ggstore_hub
 """,
 "pic": "AgACAgIAAxkBAAJM4mlOddo-JOGJwe8S8ByGTBzA57ybAAJhDmsbd-d4SrVdt_29HYnbAQADAgADeAADNgQ",
-"link": "https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot"},
+"link": "https://gg.markets/g-TeamSeek"},
 
     6: {"text": """GG.Store проводит розыгрыш призов
 Главные призы — Porsche Panamera и мощный игровой ПК.
-Прими участие 👉 https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot
+Прими участие 👉 https://gg.markets/g-TeamSeek
 Присоеденяйся: https://t.me/ggstore_hub
 """,
 "pic": "AgACAgIAAxkBAAJM5GlOdjzTsxholvr4MPUJ3zZFlP_IAAJtDmsbd-d4SrCBtHWVawABbwEAAwIAA3kAAzYE",
-"link": "https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot"},
+"link": "https://gg.markets/g-TeamSeek"},
 
     7: {"text": """Хочешь шанс выиграть Porsche Panamera?
 На GG.Store сейчас проходит розыгрыш с крупными призами.
-Все детали 👉 https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot
+Все детали 👉 https://gg.markets/g-TeamSeek
 Наш канал: https://t.me/ggstore_hub""",
 "pic": "AgACAgIAAxkBAAJM5mlOdlBgIUA6KJDMUPTLjXQrhSbYAAJuDmsbd-d4SuK4PaYdxOoGAQADAgADeAADNgQ",
-"link": "https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot"},
+"link": "https://gg.markets/g-TeamSeek"},
 
     8: {"text": """Запустили большой розыгрыш на GG.Store
-Разыгрываем Porsche Panamera, игровой ПК и много других призов. Подробнее👉 https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot
+Разыгрываем Porsche Panamera, игровой ПК и много других призов. Подробнее👉 https://gg.markets/g-TeamSeek
 Присоеденяйся: https://t.me/ggstore_hub""",
 "pic": "AgACAgIAAxkBAAJM6GlOdmH9iwt3zlTPQ7hXFov7aTGkAAJ5Dmsbd-d4SvbiDvLFGVgeAQADAgADeQADNgQ",
-"link": "https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot"},
+"link": "https://gg.markets/g-TeamSeek"},
 
     9: {"text": """GG.Store разыгрывает крутые призы
 Можно забрать Porsche Panamera, игровой ПК и многое другое.
-Участвовать легко — купи или продай 👉 https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot
+Участвовать легко — купи или продай 👉 https://gg.markets/g-TeamSeek
 Наш канал: https://t.me/ggstore_hub
 """,
 "pic": "AgACAgIAAxkBAAJM5GlOdjzTsxholvr4MPUJ3zZFlP_IAAJtDmsbd-d4SrCBtHWVawABbwEAAwIAA3kAAzYE",
-"link": "https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot"},
+"link": "https://gg.markets/g-TeamSeek"},
 
     10: {"text": """Любишь играть? Тогда зарабатывай на этом.
-На GG.Store можно продавать игровые ценности, получать деньги и участвовать в розыгрыше с шансом выиграть Porsche Panamera. Начать легко 👉 https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot
+На GG.Store можно продавать игровые ценности, получать деньги и участвовать в розыгрыше с шансом выиграть Porsche Panamera. Начать легко 👉 https://gg.markets/g-TeamSeek
 Присоеденяйся: https://t.me/ggstore_hub""",
 "pic": "AgACAgIAAxkBAAJMvmlOdJmv3aZtynXVPngOIjkc0pWtAAJKDmsbd-d4Sipxg6Pys407AQADAgADeQADNgQ",
-"link": "https://click.gg.partners/FXm9F6zx?landing=1&sub_id1=TeamSeek&sub_id2=bot"},
+"link": "https://gg.markets/g-TeamSeek"},
 }
